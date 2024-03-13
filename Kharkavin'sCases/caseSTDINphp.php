72eb84661acdea83050d3ac794e732f157e771cc
alkstsgv <alkstsgv@yandex.ru>
2024-03-13 22:05:55 +1000

    ver 3.0

--- Kharkavin'sCases/caseSTDINphp.php
+++ Kharkavin'sCases/caseSTDINphp.php
@@ -4,22 +4,39 @@ function stdin(string $inputString): array
 {
     $parsedInputString = $inputString;
     $arrayOfParsedString = [];
+    $strLen = mb_strlen($parsedInputString);
     $lenOfParsedString = "";
     $arrayIndex = 0;
     $arrayValue = 0;
+    $indexOfParsedArray = 0;
 
-    for ($i = 0; $parsedInputString[$i]; $i += 1)
-    {
-            $lenOfParsedString = "{$lenOfParsedString}{$parsedInputString[$i]}";
-            $arrayOfParsedString[$arrayIndex][$arrayValue] = $lenOfParsedString;
-            if ($parsedInputString[$i] === " "){
-                $i += 1;
-                $lenOfParsedString = "{$parsedInputString[$i]}";
-                $arrayValue += 1;
-                $arrayOfParsedString[$arrayIndex][$arrayValue] = $lenOfParsedString;
+    for ($i = 0; $i < $strLen; $i++){
+        $lenOfParsedString = "{$lenOfParsedString}{$parsedInputString[$i]}";
+        $arrayOfParsedString[$arrayIndex][$arrayValue] = trim($lenOfParsedString);
 
+        if ($parsedInputString[$i] === " "){
+            $lenOfParsedString = "";
+            $arrayValue += 1;
+        }
+
+    }
+
+    foreach ($arrayOfParsedString as $index => $value){
+        foreach ($value as $item) {
+            if ($value[$indexOfParsedArray] === "0<") {
+                //print_r($value[$indexOfParsedArray]);
+                unset($arrayOfParsedString[$index][$indexOfParsedArray]);
+            }
+            $indexOfParsedArray += 1;
         }
     }
-    return  $arrayOfParsedString;
+
+    return $arrayOfParsedString;
+
 }
-var_dump(stdin("grep 1< myfile.txt"));
+
+
+$inputString = "grep 0< myfile.txt 0< 0<";
+var_dump(stdin($inputString));
+
+
---
+++ Kharkavin'sCases/notGood.php
@@ -0,0 +1,75 @@
+<?php
+declare(strict_types = 1);
+function stdin(string $inputString)
+{
+    $parsedInputString = $inputString;
+    $arrayOfParsedString = [];
+    $strLen = lenOfStr($parsedInputString);
+    $lenOfParsedString = "";
+    $arrayIndex = 0;
+    $arrayValue = 0;
+    /* for ($i = 0; $parsedInputString[$i]; $i += 1)
+     {
+             $lenOfParsedString = "{$lenOfParsedString}{$parsedInputString[$i]}";
+             $arrayOfParsedString[$arrayIndex][$arrayValue] = $lenOfParsedString;
+             if ($parsedInputString[$i] === " "){
+                 $i += 1;
+                 $lenOfParsedString = "{$parsedInputString[$i]}";
+                 $arrayValue += 1;
+                 $arrayOfParsedString[$arrayIndex][$arrayValue] = $lenOfParsedString;
+
+             }
+
+     }*/
+
+    for ($i = 0; $i < $strLen; $i++){
+        $lenOfParsedString = "{$lenOfParsedString}{$parsedInputString[$i]}";
+        $arrayOfParsedString[$arrayIndex][$arrayValue] = $lenOfParsedString;
+        if ($parsedInputString[$i] === " "){
+            //$i += 1;
+            $lenOfParsedString = "{$parsedInputString[$i]}";
+            $arrayValue += 1;
+            $arrayOfParsedString[$arrayIndex][$arrayValue] = $lenOfParsedString;
+
+        }
+
+    }
+
+    //return $lenOfParsedString;
+    return  $arrayOfParsedString;
+
+}
+
+function lenOfStr(string $stringToCount): int
+{
+    $index = 0;
+    for ($i = 0; $stringToCount[$i] ?? null; $i += 1) {
+        $index += 1;
+
+    }
+
+    return $index;
+}
+
+function trimStr(string $stringToTrim): string
+{
+    $resultedString = "";
+    $strLen = lenOfStr($stringToTrim);
+
+    for ($i = 0; $i < $strLen; $i++){
+        $index = $i;
+        $resultedString = "{$resultedString}{$stringToTrim[$index]}";
+        if ($stringToTrim[$index] === " "){
+            $resultedString = "";
+            //$stringToTrim[$index] = $resultedString;
+        }
+    }
+    return $resultedString;
+}
+
+$inputString = "grep 0< myfile.txt ";
+//var_dump(stdin($inputString));
+var_dump(lenOfStr($inputString));
+//var_dump(trimStr($inputString));
+
+//echo trim($inputString);
