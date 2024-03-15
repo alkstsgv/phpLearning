10d5ad4af2aa6126fc8a4e93498ebdd7876e4efa
alkstsgv <alkstsgv@yandex.ru>
2024-03-15 20:28:32 +1000

    ver 4.0

--- Kharkavin'sCases/caseSTDINphp.php
+++ Kharkavin'sCases/caseSTDINphp.php
@@ -9,18 +9,14 @@ function stdin(string $inputString): array
     $arrayIndex = 0;
     $arrayValue = 0;
     $indexOfParsedArray = 0;
-
     for ($i = 0; $i < $strLen; $i++){
         $lenOfParsedString = "{$lenOfParsedString}{$parsedInputString[$i]}";
         $arrayOfParsedString[$arrayIndex][$arrayValue] = trim($lenOfParsedString);
-
         if ($parsedInputString[$i] === " "){
             $lenOfParsedString = "";
             $arrayValue += 1;
         }
-
     }
-
     foreach ($arrayOfParsedString as $index => $value){
         foreach ($value as $item) {
             if ($value[$indexOfParsedArray] === "0<") {
@@ -30,51 +26,37 @@ function stdin(string $inputString): array
             $indexOfParsedArray += 1;
         }
     }
-
     return $arrayOfParsedString;
-
 }
-
 function checkCases($func, array $cases): void
 {
-
-    print_r($func);
-    print_r($cases);
-    foreach ($cases as [$args, $expectedResult]){
-        print_r($args);
-        $r = $func(...$args);
-        print_r($r);
+    foreach ($cases as [$args, $expectedResult]) {
+        $r = $func($args);
         //print_r($expectedResult);
-       /* if ($r !== $expectedResult){
+        print_r($r[0][0]);
+        /*
+        if ($r !== $expectedResult){
+            //print_r($r);
+            //print_r($expectedResult[0]);
             throw new \Exception("failed!!!");
         }*/
     }
 }
-
-
 /*function cases(): array
 {
     $arr = [
         ["grep1"],
         ["myfile.txt1"]
     ];
-    
+
     return $arr;
-git
 }*/
 $inputString = "grep 0< myfile.txt";
-
-
 checkCases(fn($n) => stdin($inputString),
-   [ ["grep1 0< myfile.txt"], ["grep"], ["myfile.txt"]]
+    ["grep 0< myfile.txt", ["grep1", "myfile.txt"]]
 );
-
 //var_dump(checkCases());
-
-
 //var_dump((cases("grep")));
 //var_dump((cases("0<")));
 //var_dump((cases("myfile.txt")));
 //var_dump(stdin($inputString));
-
-
