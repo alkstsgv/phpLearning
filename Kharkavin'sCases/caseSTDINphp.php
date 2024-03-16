b71bc310ec2cb2630bf79bd821f97cf44bc4b265
alkstsgv <alkstsgv@yandex.ru>
2024-03-16 17:46:19 +1000

    ver 5.0

--- Kharkavin'sCases/caseSTDINphp.php
+++ Kharkavin'sCases/caseSTDINphp.php
@@ -9,6 +9,7 @@ function stdin(string $inputString): array
     $arrayIndex = 0;
     $arrayValue = 0;
     $indexOfParsedArray = 0;
+
     for ($i = 0; $i < $strLen; $i++){
         $lenOfParsedString = "{$lenOfParsedString}{$parsedInputString[$i]}";
         $arrayOfParsedString[$arrayIndex][$arrayValue] = trim($lenOfParsedString);
@@ -17,46 +18,48 @@ function stdin(string $inputString): array
             $arrayValue += 1;
         }
     }
-    foreach ($arrayOfParsedString as $index => $value){
+    foreach ($arrayOfParsedString as $key => $value){
+
         foreach ($value as $item) {
             if ($value[$indexOfParsedArray] === "0<") {
                 //print_r($value[$indexOfParsedArray]);
-                unset($arrayOfParsedString[$index][$indexOfParsedArray]);
+                unset($arrayOfParsedString[$key][$indexOfParsedArray]);
             }
             $indexOfParsedArray += 1;
         }
     }
     return $arrayOfParsedString;
-}
-function checkCases($func, array $cases): void
-{
-    foreach ($cases as [$args, $expectedResult]) {
-        $r = $func($args);
-        //print_r($expectedResult);
-        print_r($r[0][0]);
-        /*
-        if ($r !== $expectedResult){
-            //print_r($r);
-            //print_r($expectedResult[0]);
-            throw new \Exception("failed!!!");
-        }*/
-    }
-}
-/*function cases(): array
-{
-    $arr = [
-        ["grep1"],
-        ["myfile.txt1"]
-    ];
 
-    return $arr;
-}*/
+}
+function checkCases( $func, array $cases): void
+{
+
+    //print_r($func);
+
+    foreach ($cases as [$args, $expectedResult]) {
+        //print_r($args) . PHP_EOL;
+        //print_r($expectedResult) . PHP_EOL;
+        //print_r($func(...$args));
+        $r = $func($args);
+        //print_r($r[0][2]) . PHP_EOL;
+        //print_r($expectedResult) . PHP_EOL;
+
+        if ($r !== $expectedResult){
+            throw new \Exception("failed!");
+        }
+    }
+
+}
+
 $inputString = "grep 0< myfile.txt";
+
+//var_dump(stdin($inputString));
 checkCases(fn($n) => stdin($inputString),
-    ["grep 0< myfile.txt", ["grep1", "myfile.txt"]]
+    ["myfile.txt", ["grep1", "myfile.txt"]]
 );
+
+
 //var_dump(checkCases());
 //var_dump((cases("grep")));
 //var_dump((cases("0<")));
 //var_dump((cases("myfile.txt")));
-//var_dump(stdin($inputString));
