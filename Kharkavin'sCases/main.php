e0da14502d718a4fc4df8443b71b21214cdc3619
alkstsgv <alkstsgv@yandex.ru>
2024-04-07 17:17:39 +1000

    ver 6.7

--- Kharkavin'sCases/main.php
+++ Kharkavin'sCases/main.php
@@ -1,111 +1,40 @@
 <?php declare(strict_types=1);
 
-//function cutString(string $original, string $needed): string
-//{
-//    $j = 0;
-//    for ($i = 0; $i < strlen($original);$i++){
-//        //print_r($original[$i]);
-//        if (($i <= (strlen($original) - strlen($needed))) && ($j < strlen($needed)) && ($original[$i + $j] === $needed[$j] )) {
-//            $j++;
-//
-//        } elseif ($i <= (strlen($original) - strlen($needed)) && ($j < strlen($needed))){
-//            //print_r($original[$i]);
-//            $i++;
-//            $j = 0;
-//        }
-//    }
-//    return "";
-//}
-
 function cutString(string $original, string $needed): string
 {
-    $a = 0;
-    $b = 0;
-    $j = 0;
-    $r = "";
+    $sumOfmatchesOfFirstSymbolOfSubstr = 0; // a
+    $sumOfmatchesLeavedSymbolsOfSubstr = 0; // b
+//    $sumOfFullMatchesOfWord = 0;  // c
+    $j = 0; //j
+    $result = ""; // r
     $lenOfSubstr = (int)strlen($needed);
 
     for ($i = 0; $i < strlen($original); $i++) {
         for ($j = 0; $j < $lenOfSubstr; $j++) {
             if ($needed[0] === $original[$i] && $j === 0) {
-
-                $a++;
-                $r .= $original[$i];
-//                print_r($original[$i] . " " . $i . " " . $needed[$j] . " " . $j . PHP_EOL);
-                if ($a === $lenOfSubstr - 1){
-//                    print_r("yes" . "{$a}");
-                }
-            }
-            if ($needed[$j] === $original[$i] && $j !== 0 && $a !== 0) {
-                $b++;
-                $r .= $original[$i];
-//                if ($b === $lenOfSubstr - 1){
-////                    print_r("yes" . "{$a}");
-//                }
-//                print_r($original[$i] . " " . $i . " " . $needed[$j] . " " . $j . PHP_EOL);
+                $sumOfmatchesOfFirstSymbolOfSubstr++;
+                $result .= $original[$i];
                 break;
             }
-            if ($j === $lenOfSubstr - 1){
-                print_r($original[$i] . " " . $i . " " . $needed[$j] . " " . $j . PHP_EOL);
-
+            if ($needed[$j] === $original[$i] && $j !== 0 && $sumOfmatchesOfFirstSymbolOfSubstr !== 0) {
+                $sumOfmatchesLeavedSymbolsOfSubstr++;
+                $result .= $original[$i];
+                if ($sumOfmatchesOfFirstSymbolOfSubstr === 1 && $sumOfmatchesLeavedSymbolsOfSubstr === $lenOfSubstr - 1){
+                    $sumOfmatchesOfFirstSymbolOfSubstr = 0;
+                    $sumOfmatchesLeavedSymbolsOfSubstr = 0;
+                     break;
+                } else {
+//                    $sumOfFullMatchesOfWord++;
+                }
+                break;
             }
+
 
         }
     }
-    return $r;
+    return $result;
 }
-var_dump(cutString("Wowoowoowoow", "woo"));
-
-//function cutString(string $original, string $needed): string
-//{
-//    //$i = 0;
-//    $index = 0;
-//    $a = 0;
-//    $r = "";
-//    $lenOfSubstr = (int)strlen($needed);
-//
-//    for ($i = 0; $i < strlen($original); $i++) {
-//        if (($needed[$index] === $original[$i])) {
-//            //print_r($needed[$index] . " " . $i . " "  . PHP_EOL);
-//            print_r($original[$i] . " " . $i  . PHP_EOL);
-//            $a++;
-//            //print_r($a . " ");
-//            //print_r($original[$i] . PHP_EOL);
-//            if ($index < $lenOfSubstr){
-//                //print_r($original[$i] . " " . $i . PHP_EOL);
-//                $index++;
-//                //$i++;
-//                //continue;
-//
-//            } /*elseif ($index < $lenOfSubstr){
-//                //print_r($original[$i] . " " . $i . PHP_EOL);
-//                $index++;
-//                //$i++;
-//                continue;
-//            } */else {
-//                //print_r($original[$i] . " " . $i . PHP_EOL);
-//                $index = 0;
-//                $i = $a;
-//                //continue;
-//            }
-//        } elseif ($needed[$index] !== $original[$i]) {
-//            //print_r($needed[$index] . " " . $i . " "  . PHP_EOL);
-//            //print_r($original[$i] . " " . $i . PHP_EOL);
-//            $a++;
-//            //print_r($a . PHP_EOL);
-//            $index = 0;
-//            $i = $a;
-//        }
-//
-//        //print_r($a);
-//    }
-//
-//
-//    return $r;
-//}
-
-
-
+var_dump(cutString("123Wowoowoow", "woo"));
 
 //function checkCases(callable $func, array $cases): void
 //{
